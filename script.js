daacd88f1a648795a3d2f33a0e875a22f5a60c19
Chris Hollis <chrisbhollis@gmail.com>
2022-08-05 12:13:52 -0700

    changes to index scipt styles

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-First name: <input id="first_name">
-Last name: <input id="last_name">
-<button id="say">Say hi!</button>
- 
-<hr>
-<div id="result"></div>
- 
-<script>
-function say_hi() {
-    var fname = document.getElementById('first_name').value;
-    var lname = document.getElementById('last_name').value;
- 
-    var html = 'Hello <b>' + fname + '</b> ' + lname;
- 
-    document.getElementById('result').innerHTML = html;
+const Yes = "One Love!"
+const No = "No Love!"
+
+let response = prompt("Do you like Reggea Music?");
+let nameInput = prompt("What is your name?");
+document.write("Hello, " + nameInput + " welcome to my site!");
+console.log(response);
+
+if (response == "Yes"){
+  document.write("One Love!");}
+  else if (response == "No"){
+  document.write("No Love!");} 
+  else {document.write("You Found Love!");
 }
- 
-document.getElementById('say').addEventListener('click', say_hi);
-</script>
